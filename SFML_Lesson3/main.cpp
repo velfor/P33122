aa2db5e6933242703d9e70e70c912bd14d642acd
Paul Nikiforoff <velfor@tut.by>
2022-12-07 18:41:42 +0300

    Add files via upload

--- SFML_Lesson3/main.cpp
+++ SFML_Lesson3/main.cpp
@@ -1,92 +1,71 @@
+#include <SFML/Graphics.hpp>
 #include "settings.h"
+
 using namespace sf;
 
 int main()
 {
-	srand(time(nullptr));
-	// Объект, который, собственно, является главным окном приложения
 	RenderWindow window(VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), WINDOW_TITLE);
 	window.setFramerateLimit(FPS);
-	//создание объектов игры
-	
-	//ракетки
+
+	//Создание объектов
 	RectangleShape leftBat, rightBat;
-	leftBat.setSize(batSize);
-	rightBat.setSize(batSize);
-	leftBat.setFillColor(leftBatColor);
-	rightBat.setFillColor(rightBatColor);
-	leftBat.setPosition(batOffset, (WINDOW_HEIGHT - batHeight) / 2);
-	rightBat.setPosition(WINDOW_WIDTH - batOffset, (WINDOW_HEIGHT - batHeight) / 2);
-	float leftBatSpeedY = 0.f;
-	float rightBatSpeedY = 0.f;
+	leftBat.setSize(BAT_SIZE);
+	leftBat.setFillColor(LEFT_BAT_COLOR);
+	leftBat.setPosition(BAT_OFFSET, (WINDOW_HEIGHT - BAT_SIZE.y) / 2);
+	rightBat.setSize(BAT_SIZE);
+	rightBat.setFillColor(RIGHT_BAT_COLOR);
+	rightBat.setPosition(WINDOW_WIDTH - BAT_OFFSET - BAT_SIZE.x, (WINDOW_HEIGHT - BAT_SIZE.y) / 2);
 
-	//шарик
-	CircleShape ball(ballRadius);
-	ball.setFillColor(ballColor);
-	ball.setPosition((WINDOW_WIDTH - 2*ballRadius) / 2, 
-		(WINDOW_HEIGHT - 2 * ballRadius) / 2);
+	CircleShape ball(BALL_RADIUS);
+	ball.setFillColor(BALL_COLOR);
+	ball.setPosition((WINDOW_WIDTH - 2 * BALL_RADIUS) / 2, 
+		(WINDOW_HEIGHT - 2 * BALL_RADIUS) / 2);
 
-	float arr_speed[]{ -5.f,-4.f,-3.f,-2.f,-1.f,1.f,2.f,3.f,4.f,5.f };
-	int index = rand() % 10;
-	float ball_speedx = arr_speed[index];
-	index = rand() % 10;
-	float ball_speedy = arr_speed[index];
-	
-	
-	// Главный цикл приложения. Выполняется, пока открыто окно
+	float ballSpeedX = 2.f;
+	float ballSpeedY = 3.f;
+
 	while (window.isOpen())
 	{
-		//1 Обрабатываем очередь событий в цикле
+		//1 обработка событий
 		Event event;
 		while (window.pollEvent(event))
 		{
-			// Пользователь нажал на «крестик» и хочет закрыть окно?
 			if (event.type == Event::Closed)
-				// тогда закрываем его
 				window.close();
 		}
-
-		//2 Обновление объектов
-		//шарик
-		//движение шарика
-		ball.move(ball_speedx, ball_speedy);
-		//попал-вышел за левую или правую границу
-		if (ball.getPosition().x <= 0 || ball.getPosition().x >=
-			(WINDOW_WIDTH - 2 * ballRadius))
+		// Обновление игровых объектов
+		//MЯЧ
+		ball.move(ballSpeedX, ballSpeedY);
+		if (ball.getPosition().x <= 0 ||
+			ball.getPosition().x + 2 * BALL_RADIUS >= WINDOW_WIDTH)
 		{
-			//скорость по оси х сменилась на противоположную
-			ball_speedx = -ball_speedx;
+			ballSpeedX = -ballSpeedX;
 		}
-		if (ball.getPosition().y <= 0 || ball.getPosition().y >=
-			(WINDOW_HEIGHT - 2 * ballRadius))
+		if (ball.getPosition().y <= 0 ||
+			ball.getPosition().y + 2 * BALL_RADIUS >= WINDOW_HEIGHT)
 		{
-			//скорость по оси y сменилась на противоположную
-			ball_speedy = -ball_speedy;
+			ballSpeedY = -ballSpeedY;
 		}
-
-		//Проверка нажатий клавиш
-		//если клавиша W нажата - лева ракетка вниз
+		//ЛЕВАЯ РАКЕТКА
 		if (Keyboard::isKeyPressed(Keyboard::W)) {
-			leftBatSpeedY = -batSpeed;
+			leftBat.move(0.f, -BAT_DY);
 		}
-		if (Keyboard::isKeyPressed(Keyboard::S)) {
-			leftBatSpeedY = batSpeed;
+		else if (Keyboard::isKeyPressed(Keyboard::S)) {
+			leftBat.move(0.f, BAT_DY);
 		}
-		//двигаем ракетку		
-		leftBat.move(0, leftBatSpeedY);
-		//и сразу обнуляем скорость
-		leftBatSpeedY = 0;
-
-
-
-		//3 Отрисовка окна
-		//3.1 Очистка окна
+		//ПРАВАЯ РАКЕТКА
+		if (Keyboard::isKeyPressed(Keyboard::Up)) {
+			leftBat.move(0.f, -BAT_DY);
+		}
+		else if (Keyboard::isKeyPressed(Keyboard::Down)) {
+			leftBat.move(0.f, BAT_DY);
+		}
+		//3 отрисовка объектов и обновление окна
 		window.clear();
-		//3.2 Отрисовка объектов (В ПАМЯТИ!)
 		window.draw(leftBat);
 		window.draw(rightBat);
 		window.draw(ball);
-		//3.3 вывод на экран
 		window.display();
 	}
 
--- SFML_Lesson3/settings.h
+++ SFML_Lesson3/settings.h
@@ -1,18 +1,17 @@
 #pragma once
-#include <SFML/Graphics.hpp>
+#include <string>
 
-const float WINDOW_WIDTH = 800;
-const float WINDOW_HEIGHT = 600;
-const std::string WINDOW_TITLE = "SFML Lesson2";
-const float FPS = 60.f;
+// ŒÕ—“¿Õ“€
+const float WINDOW_WIDTH{ 800.f };
+const float WINDOW_HEIGHT{ 600.f };
+const std::string WINDOW_TITLE{ "SFML Lesson3" };
+const unsigned int FPS{ 60 };
 
-const float batWidth = 20.f;
-const float batHeight = 80.f;
-const float batOffset = 50.f;
-const sf::Vector2f batSize(20.f, 80.f);
-const sf::Color leftBatColor{ 158,20,37 };
-const sf::Color rightBatColor{ 20,158,49 };
-const float batSpeed = 5.f;
+const sf::Vector2f BAT_SIZE{ 25.f, 100.f };
+const float BAT_OFFSET{ 50.f };
+const sf::Color LEFT_BAT_COLOR{ 178, 38, 38 };
+const sf::Color RIGHT_BAT_COLOR{ 255, 205, 50 };
+const float BAT_DY = 5.f;
 
-const float ballRadius = 25.f;
-const sf::Color ballColor{ sf::Color::Yellow };
+const float BALL_RADIUS = 15.f;
+const sf::Color BALL_COLOR{ 250, 208, 0 };
