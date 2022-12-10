118191f96fe7238f3587bc936e72c0c995795777
Paul Nikiforoff <velfor@tut.by>
2022-12-10 19:31:36 +0300

    Add files via upload

--- SFML_Lesson3/main.cpp
+++ SFML_Lesson3/main.cpp
@@ -1,71 +1,123 @@
-#include <SFML/Graphics.hpp>
 #include "settings.h"
-
 using namespace sf;
 
 int main()
 {
+	srand(time(nullptr));
+	// Объект, который, собственно, является главным окном приложения
 	RenderWindow window(VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), WINDOW_TITLE);
 	window.setFramerateLimit(FPS);
-
-	//Создание объектов
+	//создание объектов игры
+	
+	//ракетки
 	RectangleShape leftBat, rightBat;
-	leftBat.setSize(BAT_SIZE);
-	leftBat.setFillColor(LEFT_BAT_COLOR);
-	leftBat.setPosition(BAT_OFFSET, (WINDOW_HEIGHT - BAT_SIZE.y) / 2);
-	rightBat.setSize(BAT_SIZE);
-	rightBat.setFillColor(RIGHT_BAT_COLOR);
-	rightBat.setPosition(WINDOW_WIDTH - BAT_OFFSET - BAT_SIZE.x, (WINDOW_HEIGHT - BAT_SIZE.y) / 2);
+	leftBat.setSize(batSize);
+	rightBat.setSize(batSize);
+	leftBat.setFillColor(leftBatColor);
+	rightBat.setFillColor(rightBatColor);
+	leftBat.setPosition(batOffset, (WINDOW_HEIGHT - batHeight) / 2);
+	rightBat.setPosition(WINDOW_WIDTH - batOffset, (WINDOW_HEIGHT - batHeight) / 2);
+	float leftBatSpeedY = 0.f;
+	float rightBatSpeedY = 0.f;
 
-	CircleShape ball(BALL_RADIUS);
-	ball.setFillColor(BALL_COLOR);
-	ball.setPosition((WINDOW_WIDTH - 2 * BALL_RADIUS) / 2, 
-		(WINDOW_HEIGHT - 2 * BALL_RADIUS) / 2);
+	//шарик
+	CircleShape ball(ballRadius);
+	ball.setFillColor(ballColor);
+	ball.setPosition((WINDOW_WIDTH - 2*ballRadius) / 2, 
+		(WINDOW_HEIGHT - 2 * ballRadius) / 2);
 
-	float ballSpeedX = 2.f;
-	float ballSpeedY = 3.f;
+	float arr_speed[]{ -5.f,-4.f,-3.f,-2.f,-1.f,1.f,2.f,3.f,4.f,5.f };
+	int index = rand() % 10;
+	float ball_speedx = arr_speed[index];
+	index = rand() % 10;
+	float ball_speedy = arr_speed[index];
 
+	//СЧЕТ
+	int leftPlayerScore = 0;
+	int rightPlayerScore = 0;
+
+	Font font;
+	font.loadFromFile("ds-digib.ttf");
+
+	Text leftPlayerScoreText;
+	leftPlayerScoreText.setString(std::to_string(leftPlayerScore));//передать строку для отображения
+	leftPlayerScoreText.setFont(font);
+	leftPlayerScoreText.setCharacterSize(64);
+	leftPlayerScoreText.setPosition(200.f, 10.f);
+
+	Text rightPlayerScoreText;
+	rightPlayerScoreText.setString(std::to_string(rightPlayerScore));
+	rightPlayerScoreText.setFont(font);
+	rightPlayerScoreText.setCharacterSize(64);
+	rightPlayerScoreText.setPosition(550.f, 10.f);
+	
+	// Главный цикл приложения. Выполняется, пока открыто окно
 	while (window.isOpen())
 	{
-		//1 обработка событий
+		//1 Обрабатываем очередь событий в цикле
 		Event event;
 		while (window.pollEvent(event))
 		{
+			// Пользователь нажал на «крестик» и хочет закрыть окно?
 			if (event.type == Event::Closed)
+				// тогда закрываем его
 				window.close();
 		}
-		// Обновление игровых объектов
-		//MЯЧ
-		ball.move(ballSpeedX, ballSpeedY);
-		if (ball.getPosition().x <= 0 ||
-			ball.getPosition().x + 2 * BALL_RADIUS >= WINDOW_WIDTH)
+
+		//2 Обновление объектов
+		//шарик
+		//движение шарика
+		ball.move(ball_speedx, ball_speedy);
+		//попал-вышел за левую 
+		if (ball.getPosition().x <= 0)
 		{
-			ballSpeedX = -ballSpeedX;
+			//скорость по оси х сменилась на противоположную
+			ball_speedx = -ball_speedx;
+			//правый игрок забил гол
+			rightPlayerScore++;
+			rightPlayerScoreText.setString(std::to_string(rightPlayerScore));
 		}
-		if (ball.getPosition().y <= 0 ||
-			ball.getPosition().y + 2 * BALL_RADIUS >= WINDOW_HEIGHT)
+		//или правую границу
+		if (ball.getPosition().x >=(WINDOW_WIDTH - 2 * ballRadius))
 		{
-			ballSpeedY = -ballSpeedY;
+			//скорость по оси х сменилась на противоположную
+			ball_speedx = -ball_speedx;
+			//левый игрок забил гол
+			leftPlayerScore++;
+			leftPlayerScoreText.setString(std::to_string(leftPlayerScore));
 		}
-		//ЛЕВАЯ РАКЕТКА
+		if (ball.getPosition().y <= 0 || ball.getPosition().y >=
+			(WINDOW_HEIGHT - 2 * ballRadius))
+		{
+			//скорость по оси y сменилась на противоположную
+			ball_speedy = -ball_speedy;
+		}
+
+		//Проверка нажатий клавиш
+		//если клавиша W нажата - лева ракетка вниз
 		if (Keyboard::isKeyPressed(Keyboard::W)) {
-			leftBat.move(0.f, -BAT_DY);
+			leftBatSpeedY = -batSpeed;
 		}
-		else if (Keyboard::isKeyPressed(Keyboard::S)) {
-			leftBat.move(0.f, BAT_DY);
+		if (Keyboard::isKeyPressed(Keyboard::S)) {
+			leftBatSpeedY = batSpeed;
 		}
-		//ПРАВАЯ РАКЕТКА
-		if (Keyboard::isKeyPressed(Keyboard::Up)) {
-			leftBat.move(0.f, -BAT_DY);
-		}
-		else if (Keyboard::isKeyPressed(Keyboard::Down)) {
-			leftBat.move(0.f, BAT_DY);
-		}
-		//3 отрисовка объектов и обновление окна
+		//двигаем ракетку		
+		leftBat.move(0, leftBatSpeedY);
+		//и сразу обнуляем скорость
+		leftBatSpeedY = 0;
+
+
+
+		//3 Отрисовка окна
+		//3.1 Очистка окна
 		window.clear();
+		//3.2 Отрисовка объектов (В ПАМЯТИ!)
 		window.draw(leftBat);
 		window.draw(rightBat);
 		window.draw(ball);
+		window.draw(leftPlayerScoreText);
+		window.draw(rightPlayerScoreText);
+		//3.3 вывод на экран
 		window.display();
 	}
 
